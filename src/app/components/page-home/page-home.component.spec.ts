3bd16310b24d3a28b8eb5c46b6d6d146ace357d0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-10 21:33:08 +0900

    reformat code

--- src/app/components/page-home/page-home.component.spec.ts
+++ src/app/components/page-home/page-home.component.spec.ts
@@ -1,8 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { PageHomeComponent } from './page-home.component';
+import { PageHomeComponent } from "./page-home.component";
 
-describe('PageHomeComponent', () => {
+describe("PageHomeComponent", () => {
 
   let component: PageHomeComponent;
   let fixture: ComponentFixture<PageHomeComponent>;
@@ -21,7 +21,7 @@ describe('PageHomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
--- src/app/components/page-home/page-home.component.ts
+++ src/app/components/page-home/page-home.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-page-home',
-  styleUrls: ['./page-home.component.scss'],
-  templateUrl: './page-home.component.html',
+  selector: "app-page-home",
+  styleUrls: ["./page-home.component.scss"],
+  templateUrl: "./page-home.component.html",
 })
 export class PageHomeComponent implements OnInit {
 

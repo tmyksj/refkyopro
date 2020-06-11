b2414f9f6d35f078de0ebcfd46d070a81e358827
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-11 19:54:51 +0900

    add library domain

---
+++ src/app/domains/library/library.domain.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from "@angular/core/testing";
+
+import { LibraryDomain } from "./library.domain";
+
+describe("LibraryDomain", () => {
+
+  let service: LibraryDomain;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LibraryDomain);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+});

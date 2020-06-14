3594234b9e1d77aff3e572bffa55f441cbc5718b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-14 20:54:00 +0900

    markdown の取得

--- src/app/domains/library/library.domain.spec.ts
+++ src/app/domains/library/library.domain.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 
 import { LibraryDomain } from "./library.domain";
@@ -7,7 +8,12 @@ describe("LibraryDomain", () => {
   let service: LibraryDomain;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+    });
+
     service = TestBed.inject(LibraryDomain);
   });
 
--- src/app/domains/library/library.domain.ts
+++ src/app/domains/library/library.domain.ts
@@ -1,11 +1,20 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class LibraryDomain {
 
-  public constructor() {
+  private httpClient: HttpClient;
+
+  public constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  public get(key: string, lang: string): Observable<string> {
+    return this.httpClient.get(`assets/library/${key}/${lang}.md`, { responseType: "text" });
   }
 
 }

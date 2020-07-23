9bb6a0b95494822744344982b19f30e06ffe640c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-07-23 14:01:18 +0900

    構造が簡潔になるように reformat

--- src/app/components/part-container-main/part-container-main.component.ts
+++ src/app/components/part-container-main/part-container-main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-part-container-main",
@@ -7,10 +7,38 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PartContainerMainComponent implements OnInit {
 
+  @Input()
+  public margin: "none" | "auto";
+
+  @Input()
+  public width: "50%" | "75%" | "100%";
+
   public constructor() {
+    this.margin = "auto";
+    this.width = "75%";
   }
 
   public ngOnInit(): void {
+  }
+
+  public sMargin(): string {
+    switch (this.margin) {
+      case "none":
+        return "0";
+      case "auto":
+        return "2rem";
+    }
+  }
+
+  public sPaddingRight(): string {
+    switch (this.width) {
+      case "50%":
+        return "50%";
+      case "75%":
+        return "25%";
+      case "100%":
+        return "0";
+    }
   }
 
 }

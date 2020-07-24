ecdf545ed340f55719f9258f7045a64eca045d6b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-07-24 10:25:42 +0900

    language domain を実装

---
+++ src/app/domains/language/dtos/language-index/language-index.dto.ts
@@ -0,0 +1,7 @@
+import { LanguageItemDto } from "../language-item/language-item.dto";
+
+export interface LanguageIndexDto {
+
+  readonly itemList: LanguageItemDto[];
+
+}
--- src/app/domains/language/language.domain.ts
+++ src/app/domains/language/language.domain.ts
@@ -1,11 +1,28 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+import { LanguageIndexDto } from "./dtos/language-index/language-index.dto";
+import { LanguageItemDto } from "./dtos/language-item/language-item.dto";
 
 @Injectable({
   providedIn: "root",
 })
 export class LanguageDomain {
 
-  public constructor() {
+  private httpClient: HttpClient;
+
+  public constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  public fetchItemList(): Observable<LanguageItemDto[]> {
+    return this.httpClient.get<LanguageIndexDto>("assets/language/index.json").pipe(
+      map<LanguageIndexDto, LanguageItemDto[]>((value: LanguageIndexDto): LanguageItemDto[] => {
+        return value.itemList;
+      }),
+    );
   }
 
 }
